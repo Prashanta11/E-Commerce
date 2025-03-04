9ae7963067bac2f4cbbd6724855f36e1a7a12834
Prashanta Deuja <prashantadeuja@gmail.com>
2025-03-04 20:03:18 +0545

    add framer-motion package; include new image carousel and review section in Home page

--- e-commerce/src/pages/Home.tsx
+++ e-commerce/src/pages/Home.tsx
@@ -2,6 +2,8 @@ import Hero from "../components/home/Hero";
 import CategoryNav from "../components/home/CategoryNav";
 import LatestProducts from "../components/home/LatestProducts";
 import FashionHero from "../components/home/FashionHero";
+import ImageCarousel from "../components/home/ImageCarousel";
+import ReviewSection from "../components/home/ReviewSection";
 
 const Home = () => {
   return (
@@ -10,6 +12,8 @@ const Home = () => {
       <CategoryNav />
       <LatestProducts />
       <FashionHero />
+      <ReviewSection />
+      <ImageCarousel />
     </div>
   );
 };
